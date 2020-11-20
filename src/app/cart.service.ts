6d836b1a7779995ad6eb079462005f8195c436a3
lilytava2510 <lilytavares10@gmail.com>
2020-11-20 12:49:36 -0500

    data done

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
@@ -19,6 +19,23 @@ export class CartService {
     return this.items;
   }
 
-  constructor() { }
+  constructor(    private http: HttpClient
+) { }
+ addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
 
 }
